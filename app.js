74b59da4e3ccd4d4def22461ace5c6980bc8faf5
frostbyte178 <frostbyte178@gmail.com>
2025-07-19 21:39:42 -0700

    laser drawing + tps limit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const canvas = document.getElementById("simulationCanvas");
 const ctx = canvas.getContext("2d");
-const mouseTracker = document.querySelector("*");
+const sidetabIds = ["infoButton", "configButton", "infoWrapper", "configWrapper"];
+const fps = 40;
+let lastRefresh = -1;
 
 // Armor constants and classes
-let beamWidth, leftX, topY;
+let beamWidth, armorLeftX, armorTopY, armorMiddleY;
 const blockStats = {
     wood:  {hp: 960,  flammability: 80, resistance: 10, color: '#c9b069'},
     stone: {hp: 1200, flammability: 0,  resistance: 50, color: '#a39260'},
@@ -105,18 +107,18 @@ class ArmorWall {
         // Draw as translucent fill if dead
         if (dead) {
             ctx.globalAlpha = 0.5;
-            ctx.fillRect(leftX + x * beamWidth, topY + y * beamWidth, beamWidth, beamWidth * height);
+            ctx.fillRect(armorLeftX + x * beamWidth, armorTopY + y * beamWidth, beamWidth, beamWidth * height);
             ctx.globalAlpha = 1;
         } else {
-            ctx.rect(leftX + x * beamWidth, topY + y * beamWidth, beamWidth, beamWidth * height);
+            ctx.rect(armorLeftX + x * beamWidth, armorTopY + y * beamWidth, beamWidth, beamWidth * height);
             ctx.fill();
             ctx.stroke();
 
             // Add lines between to turn the column into a grid
             for (let i = 1; i < height; i++) {
-                let lineY = topY + (y + i) * beamWidth;
-                ctx.moveTo(leftX + x * beamWidth, lineY);
-                ctx.lineTo(leftX + (x + 1) * beamWidth, lineY);
+                let lineY = armorTopY + (y + i) * beamWidth;
+                ctx.moveTo(armorLeftX + x * beamWidth, lineY);
+                ctx.lineTo(armorLeftX + (x + 1) * beamWidth, lineY);
                 ctx.stroke();
             }
         }
@@ -126,6 +128,7 @@ class ArmorWall {
         if (this.armorWall == undefined | this.armorWall.length == 0) return;
 
         // Set stroke and fill based on armor block color
+        ctx.beginPath();
         let color = blockStats[config.armor].color;
         ctx.fillStyle = color;
         ctx.strokeStyle = mergeColors(color, '#080808');
@@ -168,6 +171,7 @@ config = {
     inaccuracy: 0.05,
     expansion: 10000,
 }
+let singleShotDamage = 25;
 
 function updateConfig() {
     // Loop through ids based on config variable names to get data
@@ -181,7 +185,14 @@ function updateConfig() {
 
         // Save value to config
         config[item] = document.getElementById(documentId).value;
+        if (item != 'armor') {
+            // Convert all non-armor values to numbers
+            config[item] = Number(config[item]);
+        }
     }
+
+    // Save single shot damage to config based on Q count
+    singleShotDamage = config.dps / [40, 1, 2, 4, 8][config.q];
 
     // Change armor type
     armorWall.changeTo(config.armor);
@@ -191,9 +202,12 @@ function updateConfig() {
 let mouse = {
     x: 0,
     y: 0,
+    blockX: 0,
+    blockY: 0,
     lmb: false,
     rmb: false,
     held: false,
+    blockClicks: false,
 }
 let shifting = false;
 
@@ -229,6 +243,13 @@ document.addEventListener("mousedown", mouseDown);
 document.addEventListener("mouseup", mouseUp);
 document.addEventListener('keydown', updateShifting);
 document.addEventListener('keyup', updateShifting);
+
+// Disable clicking inputs from registering in sidebars
+for (let id of sidetabIds) {
+    let element = document.getElementById(id);
+    element.addEventListener('mouseover', () => {mouse.blockClicks = true});
+    element.addEventListener('mouseout', () => {mouse.blockClicks = false});
+}
 
 // Update config when any config element changes
 for(let item in config) {
@@ -269,15 +290,17 @@ function mergeColors(color1, color2) {
     return mergedColor;
 }
 
+
+
 // Draw wall of armor
 const armorMargin = 25;  // Empty margins around armor wall, in px
 function drawArmor() {
     // Get top position of armor wall in pixels to place it below the title
     let titleDiv = document.getElementById('title').getBoundingClientRect();
-    topY = titleDiv.y + titleDiv.height + armorMargin;
+    armorTopY = titleDiv.y + titleDiv.height + armorMargin;
 
     // Get height of armor wall in pixels
-    let wallHeight = ctx.canvas.height - topY - armorMargin;
+    let wallHeight = ctx.canvas.height - armorTopY - armorMargin;
 
     // Get width of armor wall in pixels
     let wallWidth = ctx.canvas.width * 0.6;
@@ -286,26 +309,122 @@ function drawArmor() {
     beamWidth = wallWidth / Math.max(10, config.thickness);
 
     // Get left position of armor wall in pixels
-    leftX = ctx.canvas.width / 2 - beamWidth * config.thickness / 2;
+    armorLeftX = ctx.canvas.width / 2 - beamWidth * config.thickness / 2;
 
-    // Dimensions of armor wall in beams
+    // Get dimensions of armor wall in beams
     let armorWidth = config.thickness;
     let armorHeight = Math.floor(wallHeight / beamWidth);
+
+    // Get middle y position of armor wall in pixels
+    armorMiddleY = armorTopY + armorHeight * beamWidth / 2;
+
+    // Get mouse position in beams
+    mouse.blockX = (mouse.x - armorLeftX) / beamWidth;
+    mouse.blockY = (mouse.y - armorMiddleY) / beamWidth;
 
     // Draw armor wall
     armorWall.generate(armorWidth, armorHeight, config.armor);
     armorWall.draw();
 }
 
+// Get height in beams or pixels based on pixel position from the left edge of the screen
+function laserBeamY(angle, pixelX, returnPixels = false) {
+    let rangeBlocks = (pixelX - armorLeftX) / beamWidth + config.range;
+    if (returnPixels)
+        return (Math.tan(angle) * rangeBlocks) * beamWidth + armorMiddleY;
+    return Math.tan(angle) * rangeBlocks;
+}
+
+function drawOldLaser() {
+    // Get base position values
+    let angle = Math.atan2(mouse.blockY, mouse.blockX + config.range);
+    angle += (Math.random() * 0.1 - 0.05) * Math.PI / 180; // Inaccuracy in radians
+
+    // Draw laser beam red outline
+    let startX = Math.max(0, armorLeftX - config.range * beamWidth);
+    let startY = laserBeamY(angle, startX, true);
+    let endY = laserBeamY(angle, ctx.canvas.width, true);
+    ctx.beginPath();
+    ctx.strokeStyle = '#f52727';
+    ctx.lineWidth = Math.min(64, beamWidth * 0.8); // Size based on beam size, max of 64px
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(ctx.canvas.width, endY);
+    ctx.stroke();
+    
+    // Draw laser beam white center
+    ctx.beginPath();
+    ctx.strokeStyle = '#dddddd';
+    ctx.lineWidth = Math.min(20, beamWidth * 0.25); // Size based on beam size, max of 20px
+    ctx.moveTo(ctx.canvas.width, endY);
+    ctx.lineTo(startX, startY);
+    ctx.stroke();
+}
+
+function drawNewLaser() {
+    // Get base position values
+    let baseAngle = Math.atan2(mouse.blockY, mouse.blockX + config.range);
+    baseAngle += (Math.random() * config.inaccuracy * 2 - config.inaccuracy) * Math.PI / 180; // Inaccuracy in radians
+
+    // Get angles for the top and bottom edges of the beam
+    let spreadAngle = Math.atan2(singleShotDamage / config.expansion, 100);
+
+    // Draw laser beam blue outline as a trapezoid
+    let startX = Math.max(0, armorLeftX - config.range * beamWidth);
+    ctx.beginPath();
+    ctx.strokeStyle = ctx.fillStyle = '#2727f5';
+    ctx.moveTo(startX, laserBeamY(baseAngle + spreadAngle, startX, true));
+    ctx.lineTo(ctx.canvas.width, laserBeamY(baseAngle + spreadAngle, ctx.canvas.width, true));
+    ctx.lineTo(ctx.canvas.width, laserBeamY(baseAngle - spreadAngle, ctx.canvas.width, true));
+    ctx.lineTo(startX, laserBeamY(baseAngle - spreadAngle, startX, true));
+    ctx.closePath();
+    ctx.fill();
+
+    // Draw laser beam white center as a trapezoid
+    ctx.beginPath();
+    ctx.strokeStyle = ctx.fillStyle = '#dddddd';
+    ctx.moveTo(startX, laserBeamY(baseAngle + spreadAngle / 2, startX, true));
+    ctx.lineTo(ctx.canvas.width, laserBeamY(baseAngle + spreadAngle / 2, ctx.canvas.width, true));
+    ctx.lineTo(ctx.canvas.width, laserBeamY(baseAngle - spreadAngle / 2, ctx.canvas.width, true));
+    ctx.lineTo(startX, laserBeamY(baseAngle - spreadAngle / 2, startX, true));
+    ctx.closePath();
+    ctx.fill();
+}
+
+function drawLaser() {
+    // Exit if clicks shouldn't register
+    if (mouse.blockClicks) return;
+
+    if (mouse.lmb) {
+        drawNewLaser();
+    } else if (mouse.rmb){
+        drawOldLaser();
+    }
+}
+
+// Poll time to see if we should advance frames
+function shouldRender() {
+    return Date.now() - lastRefresh >= 1000 / fps;
+}
+
 // Main runtime loop
 function mainLoop() {
+    // Exit loop if it's not yet time to advance to the next tick
+    requestAnimationFrame(mainLoop);
+    if (!shouldRender()) return;
+
+    // Update last refresh time
+    lastRefresh = Date.now();
+
+    // Update Q count slider label in the config menu
     updateQSlider();
 
+    // Reset and resize canvas
     ctx.canvas.width = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
 
+    // Draw the elements
     drawArmor();
-    requestAnimationFrame(mainLoop);
+    drawLaser();
 }
 
 updateConfig();
